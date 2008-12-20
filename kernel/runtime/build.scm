0ad311d67c83093707bed9333c95f638d2ff734d
Guillaume Cartier <gcartier@jazzscheme.org>
2008-12-20 08:36:02 -0500

    Added 'beta' to the various Jazz invites

--- kernel/runtime/build.scm
+++ kernel/runtime/build.scm
@@ -89,7 +89,8 @@
                    "."
                    (number->string minor)
                    "."
-                   (number->string revision))))
+                   (number->string revision)
+                   " beta")))
 
 
 ;;;

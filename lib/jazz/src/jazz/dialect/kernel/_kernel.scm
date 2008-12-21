ff5a29c9acb81b472bd161963440f28691d1632f
Guillaume Cartier <gcartier@jazzscheme.org>
2008-12-21 13:00:05 -0500

    Cosmetic change to the variables view

--- lib/jazz/src/jazz/dialect/kernel/_kernel.scm
+++ lib/jazz/src/jazz/dialect/kernel/_kernel.scm
@@ -617,7 +617,6 @@
 (native jazz.inspect-repl-context)
 (native jazz.repl)
 (native jazz.eval-within-no-winding)
-(native jazz.eval-within-no-winding)
 (native jazz.repl-result-history-add)
 (native repl-result-history-ref)
 

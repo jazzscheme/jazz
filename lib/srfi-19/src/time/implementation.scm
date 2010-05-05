1505cbb6d20854ff354dc997bf32c3cbd63077b5
Stephane Le Cornec <stephane.lecornec@gmail.com>
2010-05-05 15:29:22 -0400

    implement timezone for current-date - Windows version

--- lib/srfi-19/src/time/implementation.scm
+++ lib/srfi-19/src/time/implementation.scm
@@ -712,10 +712,20 @@
     ))
 
 
+;; added
+(define *timezone-proc*
+  (lambda () 0))
+
+(define (set-timezone-proc proc)
+  (set! *timezone-proc* proc))
+
+(define (get-timezone-proc)
+  *timezone-proc*)
+
 (cond-expand
   (gambit
     (define (tm:local-tz-offset)
-      0))
+      (*timezone-proc*)))
   (else
     ;; relies on the fact that we named our time zone accessor
     ;; differently from MzScheme's....

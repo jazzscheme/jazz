02ec9e8cfd6b819e0141d0d0403fee8e4a2082a5
Guillaume Cartier <gcartier@jazzscheme.org>
2010-08-15 09:33:20 -0400

    Moved generic profiler code into its own package

--- lib/contrib.statprof/src/statprof/implementation.scm
+++ lib/contrib.statprof/src/statprof/implementation.scm
@@ -20,135 +20,16 @@
 
 
 ;;;
-;;;; Settings
-;;;
-
-
-(jazz.define-setting default-profiler-depth
-  2)
-
-
-;;;
-;;;; Profile
-;;;
-
-
-(define (make-profile depth)
-  (%%vector 'profile depth #f 0 0 0 0 (%%make-table test: equal?) 0))
-
-
-(define (profile-depth profile)
-  (%%vector-ref profile 1))
-
-(define (profile-frame-count profile)
-  (%%vector-ref profile 2))
-
-(define (profile-frame-count-set! profile count)
-  (%%vector-set! profile 2 count))
-
-(define (profile-total-count profile)
-  (%%vector-ref profile 3))
-
-(define (profile-total-count-set! profile total)
-  (%%vector-set! profile 3 total))
-
-(define (profile-total-duration profile)
-  (%%vector-ref profile 4))
-
-(define (profile-total-duration-set! profile total)
-  (%%vector-set! profile 4 total))
-
-(define (profile-unknown-count profile)
-  (%%vector-ref profile 5))
-
-(define (profile-unknown-count-set! profile unknown)
-  (%%vector-set! profile 5 unknown))
-
-(define (profile-unknown-duration profile)
-  (%%vector-ref profile 6))
-
-(define (profile-unknown-duration-set! profile unknown)
-  (%%vector-set! profile 6 unknown))
-
-(define (profile-calls profile)
-  (%%vector-ref profile 7))
-
-(define (profile-user-data profile)
-  (%%vector-ref profile 8))
-
-(define (profile-user-data-set! profile user-data)
-  (%%vector-set! profile 8 user-data))
-
-
-;;;
-;;;; Active
-;;;
-
-
-(define *profile*
-  (make-profile (default-profiler-depth)))
-
-
-(define (active-profile)
-  *profile*)
-
-
-(define (new-profile #!key (depth #f))
-  (set! *profile* (make-profile (or depth default-profiler-depth))))
-
-
-(define (reset-profile)
-  (set! *profile* (make-profile (profile-depth *profile*))))
-
-
-(define (profile-frames profile)
-  (or (profile-frame-count *profile*) 1))
-
-
-;;;
-;;;; Counter
-;;;
-
-
-(define (seconds->milliseconds x)
-  (inexact->exact (round (* x 1000))))
-
-
-(define profiler-performance-frequency
-  #f)
-
-(set! profiler-performance-frequency
-      (lambda ()
-        1000))
-
-
-(define profiler-performance-counter
-  #f)
-
-(set! profiler-performance-counter
-      (lambda ()
-        (seconds->milliseconds (real-time))))
-
-
-(define (profiler-performance-frequency-set! proc)
-  (set! profiler-performance-frequency proc))
-
-
-(define (profiler-performance-counter-set! proc)
-  (set! profiler-performance-counter proc))
-
-
-;;;
 ;;;; Interruption
 ;;;
 
 
-(define *profile-running?*
+(define *statprof-running?*
   #f)
 
 
-(define (frame-profile)
-  (profile-frame-count-set! *profile* (+ (or (profile-frame-count *profile*) 0) 1)))
+(define (statprof-running?)
+  *statprof-running?*)
 
 
 (define profile-last-counter
@@ -158,22 +39,18 @@
   profile-user-data-set!)
 
 
-(define (start-profile)
-  (profile-last-counter-set! *profile* (profiler-performance-counter))
-  (set! *profile-running?* #t)
-  (if (%%not (%%fx= (profile-depth *profile*) 0))
+(define (start-statprof profile)
+  (profile-last-counter-set! profile (profiler-performance-counter))
+  (set! *statprof-running?* #t)
+  (if (%%not (%%fx= (profile-depth profile) 0))
       (%%interrupt-vector-set! 1 profile-heartbeat!)))
 
 
-(define (stop-profile)
+(define (stop-statprof profile)
   (%%interrupt-vector-set! 1 ##thread-heartbeat!)
-  (set! *profile-running?* #f)
-  (if (%%fx= (profile-depth *profile*) 0)
-      (profile-total-duration-set! *profile* (+ (profile-total-duration *profile*) (- (profiler-performance-counter) (profile-last-counter *profile*))))))
-
-
-(define (profile-running?)
-  *profile-running?*)
+  (set! *statprof-running?* #f)
+  (if (%%fx= (profile-depth profile) 0)
+      (profile-total-duration-set! profile (+ (profile-total-duration profile) (- (profiler-performance-counter) (profile-last-counter profile))))))
 
 
 ;;;
@@ -192,67 +69,40 @@
         (lambda (cont)
           (parameterize ((*in-profile-heartbeat?* #t))
             (##thread-heartbeat!)
-            (register-continuation cont))))))
+            (register-continuation (active-profile) cont))))))
 
 
-(define (register-continuation cont)
+(define (register-continuation profile cont)
   (define (duration)
     (let ((counter (profiler-performance-counter))
-          (last-counter (profile-last-counter *profile*)))
-      (profile-last-counter-set! *profile* counter)
+          (last-counter (profile-last-counter profile)))
+      (profile-last-counter-set! profile counter)
       (- counter last-counter)))
   
   (let ((duration (max 1 (duration)))
-        (stack (identify-stack cont (profile-depth *profile*))))
-    (profile-total-count-set! *profile* (+ (profile-total-count *profile*) 1))
-    (profile-total-duration-set! *profile* (+ (profile-total-duration *profile*) duration))
+        (stack (identify-stack cont (profile-depth profile))))
+    (profile-total-count-set! profile (+ (profile-total-count profile) 1))
+    (profile-total-duration-set! profile (+ (profile-total-duration profile) duration))
     (if (%%not stack)
         (begin
-          (profile-unknown-count-set! *profile* (+ (profile-unknown-count *profile*) 1))
-          (profile-unknown-duration-set! *profile* (+ (profile-unknown-duration *profile*) duration)))
+          (profile-unknown-count-set! profile (+ (profile-unknown-count profile) 1))
+          (profile-unknown-duration-set! profile (+ (profile-unknown-duration profile) duration)))
       (begin
-        (let ((actual (or (%%table-ref (profile-calls *profile*) stack #f)
+        (let ((actual (or (%%table-ref (profile-calls profile) stack #f)
                           (let ((record (cons 0 0)))
-                            (%%table-set! (profile-calls *profile*) stack record)
+                            (%%table-set! (profile-calls profile) stack record)
                             record))))
           (set-car! actual (+ (car actual) 1))
           (set-cdr! actual (+ (cdr actual) duration)))))))
 
 
 ;;;
-;;;; Stack
+;;;; Statprof
 ;;;
 
 
-(define (identify-stack cont depth)
-  (define (continuation-next-distinct cont creator)
-    (let ((creator-name (%%procedure-name creator)))
-      (let loop ((current-cont (%%continuation-next cont)))
-           (if current-cont
-               (let ((current-creator (%%continuation-creator current-cont)))
-                 (if (%%eq? creator-name (%%procedure-name current-creator))
-                     (loop (%%continuation-next current-cont))
-                   current-cont))
-             #f))))
-  
-  (define (identify-location locat)
-    (if locat
-        (let ((container (%%locat-container locat)))
-          (if container
-              (let ((filepos (%%position->filepos (%%locat-position locat))))
-                (let ((line (%%filepos-line filepos))
-                      (col (%%filepos-col filepos)))
-                  (%%list container line col)))
-            #f))
-      #f))
-  
-  (define (identify cont stack count)
-    (if (or (%%not cont) (and depth (%%fx>= count depth)))
-        stack
-      (let ((creator (%%continuation-creator cont))
-            (location (identify-location (%%continuation-locat cont))))
-        (identify (continuation-next-distinct cont creator)
-                  (%%cons (%%list creator location) stack)
-                  (%%fx+ count 1)))))
-  
-  (identify cont '() 0)))
+(define statprof
+  (make-profiler start-statprof stop-statprof))
+
+
+(default-profiler statprof))
---
+++ lib/profiler/src/profiler/implementation.scm
@@ -0,0 +1,249 @@
+;;;==============
+;;;  JazzScheme
+;;;==============
+;;;
+;;;; Profiler Support Implementation
+;;;
+;;;  The contents of this file are subject to the Mozilla Public License Version
+;;;  1.1 (the "License"); you may not use this file except in compliance with
+;;;  the License. You may obtain a copy of the License at
+;;;  http://www.mozilla.org/MPL/
+;;;
+;;;  Software distributed under the License is distributed on an "AS IS" basis,
+;;;  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+;;;  for the specific language governing rights and limitations under the
+;;;  License.
+;;;
+;;;  The Original Code is JazzScheme.
+;;;
+;;;  The Initial Developer of the Original Code is Guillaume Cartier.
+;;;  Portions created by the Initial Developer are Copyright (C) 1996-2008
+;;;  the Initial Developer. All Rights Reserved.
+;;;
+;;;  Contributor(s):
+;;;
+;;;  Alternatively, the contents of this file may be used under the terms of
+;;;  the GNU General Public License Version 2 or later (the "GPL"), in which
+;;;  case the provisions of the GPL are applicable instead of those above. If
+;;;  you wish to allow use of your version of this file only under the terms of
+;;;  the GPL, and not to allow others to use your version of this file under the
+;;;  terms of the MPL, indicate your decision by deleting the provisions above
+;;;  and replace them with the notice and other provisions required by the GPL.
+;;;  If you do not delete the provisions above, a recipient may use your version
+;;;  of this file under the terms of any one of the MPL or the GPL.
+;;;
+;;;  See www.jazzscheme.org for details.
+
+
+(unit protected profiler.implementation
+
+
+(declare (proper-tail-calls))
+
+
+;;;
+;;;; Settings
+;;;
+
+
+(jazz.define-setting default-profiler
+  #f)
+
+(jazz.define-setting default-profiler-depth
+  2)
+
+
+;;;
+;;;; Profiler
+;;;
+
+
+(define (make-profiler start stop)
+  (%%vector 'profiler start stop))
+
+
+(define (profiler-start profiler)
+  (%%vector-ref profiler 1))
+
+(define (profiler-stop profiler)
+  (%%vector-ref profiler 2))
+
+
+;;;
+;;;; Profile
+;;;
+
+
+(define (make-profile profiler depth)
+  (%%vector 'profile profiler depth #f 0 0 0 0 (%%make-table test: equal?) 0))
+
+
+(define (profile-profiler profile)
+  (%%vector-ref profile 1))
+
+(define (profile-depth profile)
+  (%%vector-ref profile 2))
+
+(define (profile-frame-count profile)
+  (%%vector-ref profile 3))
+
+(define (profile-frame-count-set! profile count)
+  (%%vector-set! profile 3 count))
+
+(define (profile-total-count profile)
+  (%%vector-ref profile 4))
+
+(define (profile-total-count-set! profile total)
+  (%%vector-set! profile 4 total))
+
+(define (profile-total-duration profile)
+  (%%vector-ref profile 5))
+
+(define (profile-total-duration-set! profile total)
+  (%%vector-set! profile 5 total))
+
+(define (profile-unknown-count profile)
+  (%%vector-ref profile 6))
+
+(define (profile-unknown-count-set! profile unknown)
+  (%%vector-set! profile 6 unknown))
+
+(define (profile-unknown-duration profile)
+  (%%vector-ref profile 7))
+
+(define (profile-unknown-duration-set! profile unknown)
+  (%%vector-set! profile 7 unknown))
+
+(define (profile-calls profile)
+  (%%vector-ref profile 8))
+
+(define (profile-user-data profile)
+  (%%vector-ref profile 9))
+
+(define (profile-user-data-set! profile user-data)
+  (%%vector-set! profile 9 user-data))
+
+
+(define (new-profile #!key (profiler #f) (depth #f))
+  (make-profile (or profiler default-profiler) (or depth default-profiler-depth)))
+
+
+;;;
+;;;; Active
+;;;
+
+
+(define (active-profile)
+  (make-parameter #f))
+
+
+(define (reset-profile)
+  (let ((active (active-profile)))
+    (active-profile (make-profile (profile-profiler active) (profile-depth active)))))
+
+
+;;;
+;;;; Frames
+;;;
+
+
+(define (frame-profile)
+  (profile-frame-count-set! *profile* (+ (or (profile-frame-count *profile*) 0) 1)))
+
+
+(define (profile-frames profile)
+  (or (profile-frame-count profile) 1))
+
+
+;;;
+;;;; Counter
+;;;
+
+
+(define (seconds->milliseconds x)
+  (inexact->exact (round (* x 1000))))
+
+
+(define profiler-performance-frequency
+  #f)
+
+(set! profiler-performance-frequency
+      (lambda ()
+        1000))
+
+
+(define profiler-performance-counter
+  #f)
+
+(set! profiler-performance-counter
+      (lambda ()
+        (seconds->milliseconds (real-time))))
+
+
+(define (profiler-performance-frequency-set! proc)
+  (set! profiler-performance-frequency proc))
+
+
+(define (profiler-performance-counter-set! proc)
+  (set! profiler-performance-counter proc))
+
+
+;;;
+;;;; Run
+;;;
+
+
+(define (with-profile profile thunk)
+  (start-profiler profile)
+  (parameterize ((active-profile profile))
+    (thunk))
+  (stop-profiler profile))
+
+
+(define (start-profiler profile)
+  (let ((start (profiler-start (profile-profiler *profile*))))
+    (start profile)))
+
+
+(define (stop-profiler profile)
+  (let ((stop (profiler-stop (profile-profiler *profile*))))
+    (stop profile)))
+
+
+;;;
+;;;; Stack
+;;;
+
+
+(define (identify-stack cont depth)
+  (define (continuation-next-distinct cont creator)
+    (let ((creator-name (%%procedure-name creator)))
+      (let loop ((current-cont (%%continuation-next cont)))
+           (if current-cont
+               (let ((current-creator (%%continuation-creator current-cont)))
+                 (if (%%eq? creator-name (%%procedure-name current-creator))
+                     (loop (%%continuation-next current-cont))
+                   current-cont))
+             #f))))
+  
+  (define (identify-location locat)
+    (if locat
+        (let ((container (%%locat-container locat)))
+          (if container
+              (let ((filepos (%%position->filepos (%%locat-position locat))))
+                (let ((line (%%filepos-line filepos))
+                      (col (%%filepos-col filepos)))
+                  (%%list container line col)))
+            #f))
+      #f))
+  
+  (define (identify cont stack count)
+    (if (or (%%not cont) (and depth (%%fx>= count depth)))
+        stack
+      (let ((creator (%%continuation-creator cont))
+            (location (identify-location (%%continuation-locat cont))))
+        (identify (continuation-next-distinct cont creator)
+                  (%%cons (%%list creator location) stack)
+                  (%%fx+ count 1)))))
+  
+  (identify cont '() 0)))

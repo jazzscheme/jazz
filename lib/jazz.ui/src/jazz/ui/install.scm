311af50251b4a83e69142eeb7d3ba0ff8c3ba884
Guillaume Cartier <gcartier@jazzscheme.org>
2010-12-21 11:37:58 -0500

    Added a new image style

--- lib/jazz.ui/src/jazz/ui/install.scm
+++ lib/jazz.ui/src/jazz/ui/install.scm
@@ -43,6 +43,7 @@
 (jazz.define-literal Format                jazz.ui.literals.construct-format)
 (jazz.define-literal Text-Style            jazz.ui.literals.construct-text-style)
 (jazz.define-literal Link-Style            jazz.ui.literals.construct-link-style)
+(jazz.define-literal Image-Style           jazz.ui.literals.construct-image-style)
 (jazz.define-literal Text                  jazz.ui.literals.construct-text)
 (jazz.define-literal Formatted-Text        jazz.ui.literals.construct-formatted-text)
 (jazz.define-literal Clipboard-Parcel      jazz.ui.literals.construct-clipboard-parcel)

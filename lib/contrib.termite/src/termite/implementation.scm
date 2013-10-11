78d4a0607485a9a97199c32f2ed1c7533b280c8b
Guillaume Cartier <gcartier@jazzscheme.org>
2013-10-10 20:22:45 -0400

    Hello world in termite

--- lib/contrib.termite/src/termite/implementation.scm
+++ lib/contrib.termite/src/termite/implementation.scm
@@ -3,4 +3,11 @@
 
 (##include "~~/lib/gambit#.scm")
 (##include "~~/lib/termite/termite#.scm")
-(load "~~/lib/termite/termite"))
+(load "~~/lib/termite/termite")
+
+
+(define me current-thread)
+
+
+(define send !)
+(define retrieve ?))

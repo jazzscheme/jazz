884c046b79c08b30396df4a03ac589fc2eb341a9
Guillaume Cartier <gcartier@jazzscheme.org>
2014-04-07 12:34:01 -0400

    Update for Gambit FFI changes

--- lib/gambit.ext/src/gambit/walk.scm
+++ lib/gambit.ext/src/gambit/walk.scm
@@ -426,14 +426,14 @@ end-of-code
 
 (define (##alloc-pair kind)
   ((c-lambda (scheme-object) scheme-object
-             "___SCMOBJ r = ___EXT(___make_pair) (___FIX(0), ___FIX(0), ___INT(___arg1));
+             "___SCMOBJ r = ___EXT(___make_pair) (___ps, ___FIX(0), ___FIX(0), ___INT(___arg1));
               ___EXT(___release_scmobj)(r);
               ___result = r;")
    kind))
 
 (define (##alloc-ovector len kind)
   ((c-lambda (scheme-object scheme-object) scheme-object
-             "___SCMOBJ r = ___EXT(___make_vector) (___INT(___arg1), ___FIX(0), ___INT(___arg2));
+             "___SCMOBJ r = ___EXT(___make_vector) (___ps, ___INT(___arg1), ___FIX(0), ___INT(___arg2));
               ___EXT(___release_scmobj)(r);
               ___result = r;")
    len
@@ -441,7 +441,7 @@ end-of-code
 
 (define (##alloc-bvector subtype len kind)
   ((c-lambda (scheme-object scheme-object scheme-object) scheme-object
-             "___SCMOBJ r = ___EXT(___alloc_scmobj) (___INT(___arg1), ___INT(___arg2), ___INT(___arg3));
+             "___SCMOBJ r = ___EXT(___alloc_scmobj) (___ps, ___INT(___arg1), ___INT(___arg2), ___INT(___arg3));
               ___EXT(___release_scmobj)(r);
               ___result = r;")
    subtype

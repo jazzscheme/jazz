07e4ba3be2071775883eb96ff862f032662a1e8c
Guillaume Cartier <gcartier@jazzscheme.org>
2015-04-14 22:19:41 -0400

    Add pixman mac foreign

--- foreign/mac/fontconfig/install.sh
+++ foreign/mac/fontconfig/install.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 
 install_name_tool -id @rpath/libfontconfig.dylib lib/libfontconfig.dylib
+install_name_tool -change /usr/local/lib/libfreetype.6.dylib @rpath/libfreetype.dylib lib/libfontconfig.dylib

4d647e8d745ebbab9d4834ed00ca257f616b65bf
Guillaume Cartier <gcartier@jazzscheme.org>
2015-09-04 10:11:24 -0400

    Move foreign into cairo package

--- foreign/mac/pixman/install.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-install_name_tool -id @rpath/libpixman-1.0.dylib lib/libpixman-1.0.dylib
--- foreign/mac/png/install.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-install_name_tool -id @rpath/libpng16.16.dylib lib/libpng16.16.dylib

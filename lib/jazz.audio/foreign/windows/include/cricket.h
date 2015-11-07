b082c7d795fff0c046b11fe09f3010583af76abf
Guillaume Cartier <gcartier@jazzscheme.org>
2015-11-07 13:32:12 -0500

    Initial port of audio to windows

---
+++ lib/jazz.audio/foreign/windows/include/cricket.h
@@ -0,0 +1,23 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+__declspec(dllexport) int __cdecl CricketInit();
+__declspec(dllexport) void __cdecl CricketShutdown();
+__declspec(dllexport) void __cdecl CricketUpdate();
+__declspec(dllexport) void* __cdecl CricketNewBankSound(char* path);
+__declspec(dllexport) void* __cdecl CricketNewStreamSound(char* path);
+__declspec(dllexport) void __cdecl CricketPlay(void* sound);
+__declspec(dllexport) void __cdecl CricketStop(void* sound);
+__declspec(dllexport) bool __cdecl CricketPlaying(void* sound);
+__declspec(dllexport) void __cdecl CricketPause(void* sound);
+__declspec(dllexport) void __cdecl CricketResume(void* sound);
+__declspec(dllexport) bool __cdecl CricketPaused(void* sound);
+__declspec(dllexport) void __cdecl CricketSetLoop(void* sound);
+__declspec(dllexport) void __cdecl CricketReleaseLoop(void* sound);
+__declspec(dllexport) float __cdecl CricketGetVolume(void* sound);
+__declspec(dllexport) void __cdecl CricketSetVolume(void* sound, float vol);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ lib/jazz.audio/foreign/windows/src/cricket.cpp
@@ -0,0 +1,104 @@
+#include <ck/ck.h>
+#include <ck/config.h>
+#include <ck/bank.h>
+#include <ck/sound.h>
+
+#include "cricket.h"
+
+
+CkConfig config;
+
+
+int CricketInit()
+{
+	return CkInit(&config);
+}
+
+
+void CricketShutdown()
+{
+	CkShutdown();
+}
+
+
+void CricketUpdate()
+{
+	CkUpdate();
+}
+
+
+void* CricketNewBankSound(char* path)
+{
+    CkBank* bank = CkBank::newBank(path);
+    CkSound* sound = CkSound::newBankSound(bank, 0);
+    return (void*) sound;
+}
+
+
+void* CricketNewStreamSound(char* path)
+{
+    CkSound* sound = CkSound::newStreamSound(path);
+    return (void*) sound;
+}
+
+
+void CricketPlay(void* sound)
+{
+	((CkSound*) sound)->play();
+}
+
+
+void CricketStop(void* sound)
+{
+	((CkSound*) sound)->stop();
+}
+
+
+bool CricketPlaying(void* sound)
+{
+	return ((CkSound*) sound)->isPlaying();
+}
+
+
+
+void CricketPause(void* sound)
+{
+	((CkSound*) sound)->setPaused(true);
+}
+
+
+void CricketResume(void* sound)
+{
+	((CkSound*) sound)->setPaused(false);
+}
+
+
+bool CricketPaused(void* sound)
+{
+	return ((CkSound*) sound)->isPaused();
+}
+
+
+void CricketSetLoop(void* sound)
+{
+	((CkSound*) sound)->setLoop(0, -1);
+	((CkSound*) sound)->setLoopCount(-1);
+}
+
+
+void CricketReleaseLoop(void* sound)
+{
+	((CkSound*) sound)->releaseLoop();
+}
+
+
+float CricketGetVolume(void* sound)
+{
+	return ((CkSound*) sound)->getVolume();
+}
+
+
+void CricketSetVolume(void* sound, float vol)
+{
+	((CkSound*) sound)->setVolume(vol);
+}

6350acfbc13e6c1ed8417f59cf189e1fba3649a6
Guillaume Cartier <gcartier@jazzscheme.org>
2016-03-03 16:30:33 -0500

    Add cons primitive pattern

--- lib/jazz.backend.scheme/src/jazz/backend/scheme/emit.scm
+++ lib/jazz.backend.scheme/src/jazz/backend/scheme/emit.scm
@@ -632,6 +632,7 @@
 
 (jazz:add-primitive-patterns     'scheme.language.runtime.kernel:car                  '((##car    <pair:any>)))
 (jazz:add-primitive-patterns     'scheme.language.runtime.kernel:cdr                  '((##cdr    <pair:any>)))
+(jazz:add-primitive-patterns     'scheme.language.runtime.kernel:cons                 '((##cons   <any^any:pair>)))
 (jazz:add-primitive-patterns     'scheme.language.runtime.kernel:length               '((##length <list:int>)     (##vector-length <vector:int>)          (##string-length <string:int>)))
 (jazz:add-primitive-patterns     'scheme.language.runtime.kernel:null?                '((##null?  <any:bool>)))
 

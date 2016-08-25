b8c6ff9dcf8fa88b0cfef4aa05256b4c100f4adf
Guillaume Cartier <gcartier@jazzscheme.org>
2016-08-25 13:14:07 -0400

    Add expt primitive pattern

--- lib/jazz.backend.scheme/src/jazz/backend/scheme/emit.scm
+++ lib/jazz.backend.scheme/src/jazz/backend/scheme/emit.scm
@@ -618,6 +618,7 @@
 
 (jazz:add-primitive-patterns     'scheme.language.runtime.kernel:abs                  '(                     (##flabs      <fv:fl>)))
 (jazz:add-primitive-patterns     'scheme.language.runtime.kernel:sqrt                 '(                     (##flsqrt     <fv:fl>)))
+(jazz:add-primitive-patterns     'scheme.language.runtime.kernel:expt                 '(                     (##flexpt     <fv^fv:fl>)))
 
 (jazz:add-primitive-patterns     'scheme.language.runtime.kernel:sin                  '(                     (##flsin      <fv:fl>)))
 (jazz:add-primitive-patterns     'scheme.language.runtime.kernel:cos                  '(                     (##flcos      <fv:fl>)))

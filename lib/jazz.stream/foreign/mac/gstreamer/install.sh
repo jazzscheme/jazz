b285220755895f2f854f6e3559b2ab5d63840cf9
Guillaume <gucartier@gmail.com>
2020-11-14 11:43:45 -0500

    Use -run in stream install script

--- lib/jazz.stream/foreign/mac/gstreamer/install.sh
+++ lib/jazz.stream/foreign/mac/gstreamer/install.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-/Users/cartier/Devel/jazz/app/cache/cache/develop/jazz -r relocate gstreamer /Library/Frameworks/GStreamer.framework/Versions/1.0 @rpath/gstreamer
+/Users/cartier/Devel/jazz/app/cache/cache/develop/jazz -run relocate gstreamer /Library/Frameworks/GStreamer.framework/Versions/1.0 @rpath/gstreamer

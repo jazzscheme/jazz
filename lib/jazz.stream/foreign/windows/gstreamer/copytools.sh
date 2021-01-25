f1313d6203621f19f9f80f65d59df38cab61d8c9
Guillaume Cartier <gcartier@jazzscheme.org>
2021-01-25 09:01:18 -0500

    Add gstreamer tools to windows

---
+++ lib/jazz.stream/foreign/windows/gstreamer/copytools.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+GSTREAMER=/c/gstreamer/1.0/mingw_x86_64
+
+
+#
+# bin
+#
+
+cpbin() {
+    cp $GSTREAMER/bin/$1.exe bin/$1.exe
+}
+
+cpbin gst-device-monitor-1.0
+cpbin gst-discoverer-1.0
+cpbin gst-inspect-1.0
+cpbin gst-launch-1.0
+cpbin gst-play-1.0
+cpbin gst-typefind-1.0
